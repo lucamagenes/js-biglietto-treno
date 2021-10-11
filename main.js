f365de54366cde293b1471b243f754652f61c289
lucamagenes <lucamgns33@gmail.com>
2021-10-11 16:33:54 +0200

    aggiunta funzione math.round per arrotondare i decimali

--- main.js
+++ main.js
@@ -26,19 +26,17 @@ console.log("costo per chilometri: ", costo_per_chilometri + "€");
 if (eta_passeggiero < 18) {
     let sconto_20 = ((costo_per_chilometri * 20) / 100);
     console.log("sconto del 20%: ", sconto_20 + "€");
-    let prezzo_minorenne = costo_per_chilometri - sconto_20;
-    console.log("prezzo del biglietto con sconto del 20%: ", prezzo_minorenne + "€");
+    let prezzo_minorenne = (Math.round((costo_per_chilometri - sconto_20) * 100) / 100);
+    console.log("prezzo per minorenni: ", prezzo_minorenne + "€");
+
 }
 
 
 //sconto del 40% per gli over 65
 
 if (eta_passeggiero > 65) {
-    let sconto_40 = ((costo_per_chilometri * 40) / 100);
+    let sconto_40 = (costo_per_chilometri * 40) / 100;
     console.log("sconto del 40%: ", sconto_40 + "€");
-    let prezzo_over_65 = costo_per_chilometri - sconto_40;
+    let prezzo_over_65 = (Math.round((costo_per_chilometri - sconto_40) * 100) / 100);
     console.log("prezzo over 65: ", prezzo_over_65 + "€");
 }
-
-
-//prezzo finale
