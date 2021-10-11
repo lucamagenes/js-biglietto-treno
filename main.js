45c4d872780b4ee79b07460e4368d8b81a83b651
lucamagenes <lucamgns33@gmail.com>
2021-10-11 15:21:50 +0200

    struttura a commenti del file js

---
+++ main.js
@@ -0,0 +1,16 @@
+//chiedere all'utente il numero di chilometri che vuole percorrere
+
+
+//chiedere all'utente l'età del passeggero
+
+
+//calcolare il prezzo totale del viaggio
+
+
+//sconto del 20% per i minorenni
+
+
+//sconto del 40% per gli over 65
+
+
+//prezzo finale
