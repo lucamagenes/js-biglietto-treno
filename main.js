4ced66c10050176a28fb7c94fdaf332d74ee10dc
lucamagenes <lucamgns33@gmail.com>
2021-10-11 15:58:08 +0200

    sconto del 20% per i minorenni

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //chiedere all'utente il numero di chilometri che vuole percorrere
 
-const numero_chilometri = Number(prompt("Quanti chilimetri vuoi percorrere?"));
+const numero_chilometri = Number(prompt("Quanti chilometri vuoi percorrere?"));
 console.log("numero chilometri da percorrere: ", numero_chilometri);
 
 
@@ -22,6 +22,14 @@ console.log("costo per chilometri: ", costo_per_chilometri);
 
 
 //sconto del 20% per i minorenni
+
+if (eta_passeggiero < 18) {
+    let sconto_20 = ((costo_per_chilometri * 20) / 100);
+    console.log("sconto del 20%: ", sconto_20);
+    let prezzo_minorenne = costo_per_chilometri - sconto_20;
+    console.log("prezzo del biglietto con sconto del 20%: ", prezzo_minorenne);
+}
+
 
 
 //sconto del 40% per gli over 65
