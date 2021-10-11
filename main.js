7349fa11f3ab8c5001fc9bc8b748eba585fd97a2
lucamagenes <lucamgns33@gmail.com>
2021-10-11 15:42:29 +0200

    calcolare il prezzo totale del viaggio

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 //chiedere all'utente il numero di chilometri che vuole percorrere
 
 const numero_chilometri = Number(prompt("Quanti chilimetri vuoi percorrere?"));
-console.log("numero chilometri da percorrere:", numero_chilometri);
+console.log("numero chilometri da percorrere: ", numero_chilometri);
 
 
 //chiedere all'utente l'età del passeggero
 
 const eta_passeggiero = Number(prompt("Quanti anni hai?"));
-console.log("anni utente:", eta_passeggiero);
+console.log("anni utente: ", eta_passeggiero);
+
+
+//prezzo del biglietto
+
+let prezzo_chilometro = 0.21;
 
 
 //calcolare il prezzo totale del viaggio
+
+let costo_per_chilometri = numero_chilometri * prezzo_chilometro;
+console.log("costo per chilometri: ", costo_per_chilometri);
 
 
 //sconto del 20% per i minorenni
